a696e8c903210fcee25b624805b8d27e1b951a38
Maxim <maxrap7878@mail.ru>
2018-08-17 13:10:43 +0300

    onDragEnd method

--- src/index.js
+++ src/index.js
@@ -11,14 +11,43 @@ class App extends Component{
 
 
     onDragEnd = result =>{
-        // TODO: reorder our column
+        const {destination, source, draggableId} = result;
+        if(!destination){
+            return
+        }
+
+
+        if(destination.droppable === source.droppable &&
+           destination.index === source.index
+        ){
+            return
+        }
+
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds)
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds,
+        };
+        const newState = {
+            ...this.state,
+            columns:{
+              ...this.state.columns,
+              [newColumn.id]: newColumn,
+            },
+        };
+
+        this.setState(newState);
+
 
     };
 
 render(){
     return(
 
-        <DragDropContext>
+        <DragDropContext onDragEnd={this.onDragEnd}>
             {this.state.columnOrder.map((columnId)=>{
             const column = this.state.columns[columnId];
             const tasks = column.taskIds.map( taskId => this.state.tasks[taskId]);
